cbe8a34ae6b3a65831a1159916dbf338e68b6f9f
mansha.qarib777@gmail.com <mansha.qarib777@gmail.com>
2021-05-08 13:34:41 +0500

    dashboard fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,13 +19,13 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('user.emailAddress') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @if($errors->has('email')) is-invalid @endif" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
+                                @if($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @enderror
+                                @endif
                             </div>
                         </div>
 
@@ -33,13 +33,13 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('user.password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @if($errors->has('password')) is-invalid @endif" name="password" required autocomplete="current-password">
 
-                                @error('password')
+                                @if($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @enderror
+                                @endif
                             </div>
                         </div>
 
